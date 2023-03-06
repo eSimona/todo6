0e6fe0a040a5ab52a1befa0e37654019b65a01e6
Simona Baublienė <krasula@gmail.com>
2023-03-06 22:47:26 +0200

    1. 'Templete/todo6' direktorijoje sukuriau failą main.html ten savo programai suteiksiu kitokį atvaizdavimą. 2.Redagavau .html failus, sukurti atvaizdavimo stiliui naudojau css.

--- my6todo/my6todo/settings.py
+++ my6todo/my6todo/settings.py
@@ -107,7 +107,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'lt'
 
 TIME_ZONE = 'Europe/Vilnius'
 
